c37f623cf6278b8a9c6ecc461be85c56a9a550da
Isaac Kim <myong328@gmail.com>
2022-04-12 16:14:35 -0700

    Initial Commit

---
+++ practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by Isaac Kim on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
